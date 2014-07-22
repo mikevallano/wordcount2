fdd443a9b4d5521e6d663b72b8d748f084656e17
mike vallano <mikevallano@gmail.com>
2014-07-22 11:03:32 -0600

    added name route

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-#@app.route('/<name>')
-#def hello_name(name):
-#    return "Hello {}!".format(name)
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
